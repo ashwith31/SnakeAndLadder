57d50a872e4b6dfe95d58c1fd608a03036d8dfe0
ashwith31 <ashwitash@gmail.com>
2021-08-06 19:58:11 +0530

    modified:Start position is set to zero

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -2,9 +2,14 @@ package com.SnakeAndLadder;
 
 public class SnakeAndLadder {
 
+	int start=0;      //start position is zero
+	
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Problem");
+		System.out.println("The Game is played by a single player");
+		
+		
 		
 	}
 
