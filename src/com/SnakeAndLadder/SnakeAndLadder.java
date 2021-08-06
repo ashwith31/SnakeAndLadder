4d486150395a0661d5638af8c337b111e2954f8f
ashwith31 <ashwitash@gmail.com>
2021-08-06 20:04:41 +0530

    modified:The dice is rolled

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -9,7 +9,8 @@ public class SnakeAndLadder {
 		System.out.println("Welcome to Snake and Ladder Problem");
 		System.out.println("The Game is played by a single player");
 		
-		
+		int dice=(int)Math.floor((Math.random()*10 %6 ))+1;         //dice is rolled
+		System.out.println("The value of dice is "+dice);
 		
 	}
 
