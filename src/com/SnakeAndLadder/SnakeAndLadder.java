bc0d1b18224f4aa5b1fd0c1d999756e69b044ff1
ashwith31 <ashwitash@gmail.com>
2021-08-07 14:59:02 +0530

    modified:Played the game with two players

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -1,20 +1,46 @@
 package com.SnakeAndLadder;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
 
 	final static int NO_PLAY=0;
 	final static int LADDER=1;
 	final static int SNAKE=2;
-	static int start=0;      											//start position is zero
 	final static int WINNING_POSITION=100;								//The Winning position is 100
 	public static void main(String[] args) {
-		
-		int count=0;											//stores the number of times the dice is rolled
+													
 		System.out.println("Welcome to Snake and Ladder Problem");
-		System.out.println("The Game is played by a single player");
 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the name of first player");
+		String playerOneName=sc.next();
+		int playerOneCount=func();
+		System.out.println("Number of times "+playerOneName+" rolled the dice= "+playerOneCount);
+		System.out.println("Enter the name of second player");
+		String playerTwoName=sc.next();
+		int playerTwoCount=func();
+		System.out.println("Number of times "+playerTwoName+" rolled the dice= "+playerTwoCount);
+
+		if(playerOneCount<playerTwoCount)
+		{
+			System.out.println(playerOneName+ " wins the game");
+		}
+		else
+		{
+			System.out.println(playerTwoName+ " wins the game");
+		}
+		
+		
+		System.out.println("The Game is finished....");
+	}
+	static int func()
+	{
+		int start=0;
+		int count=0;
 		while(start<WINNING_POSITION)
-		{												
+		{		
+			
 			int dice=(int)Math.floor((Math.random()*10 %6 ))+1;         //dice is rolled using random method
 			System.out.println("The value of dice is "+dice);
 			int option =(int)Math.floor((Math.random()*10 %3 ));        //option is selected using random method
@@ -30,6 +56,15 @@ public class SnakeAndLadder {
 					if(start>WINNING_POSITION)
 					{
 						start -=dice;
+					}
+					if(start !=WINNING_POSITION)
+					{
+						System.out.println("Playing again...");
+					int dice2=(int)Math.floor((Math.random()*10 %6 ))+1;         
+					if(start>WINNING_POSITION)
+					{
+						start -=dice2;
+					}
 					}
 					System.out.println("Your position is "+start);
 					break;
@@ -48,8 +83,6 @@ public class SnakeAndLadder {
 	}
 		count++;
 		}
-		System.out.println();
-		System.out.println("Dice is rolled "+count+" times to win the game");
-		System.out.println("The Game is finished....");
+		return count;
 	}
 }
