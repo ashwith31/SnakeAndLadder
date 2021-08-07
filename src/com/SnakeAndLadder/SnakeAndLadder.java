85e3e3d0181213607241caa3568bf6596d0021c3
ashwith31 <ashwitash@gmail.com>
2021-08-07 13:37:16 +0530

    modified:Added the winning position

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -5,16 +5,19 @@ public class SnakeAndLadder {
 	final static int NO_PLAY=0;
 	final static int LADDER=1;
 	final static int SNAKE=2;
-	static int start=0;      //start position is zero
-	
+	static int start=0;      											//start position is zero
+	final static int WINNING_POSITION=100;								//The Winning position is 100
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Problem");
 		System.out.println("The Game is played by a single player");
-		
-		int dice=(int)Math.floor((Math.random()*10 %6 ))+1;         //dice is rolled using random method
-		System.out.println("The value of dice is "+dice);
-		int option =(int)Math.floor((Math.random()*10 %3 ));        //option is selected using random method
+
+		while(start<=WINNING_POSITION)
+		{
+			int dice=(int)Math.floor((Math.random()*10 %6 ))+1;         //dice is rolled using random method
+			System.out.println("The value of dice is "+dice);
+			int option =(int)Math.floor((Math.random()*10 %3 ));        //option is selected using random method
+
 		switch(option)
 		{
 				case NO_PLAY:
@@ -28,10 +31,17 @@ public class SnakeAndLadder {
 				case SNAKE:
 					System.out.println("You got a SNAKE");
 					start -=dice;
+					if(start<0)
+					{
+						start=0;
+					}
 					System.out.println("Your position is "+start);
+					
 					break;
 				default:
 					break;	
 	}
+		}
+		System.out.println("The Game is finished....");
 	}
 }
